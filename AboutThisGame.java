7a3a004a30f327740cc48d3aae7f558607a796ca
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-04-04 10:55:37 -0400

    Create basic menu, about and howtoplay
    We'll make the AI follow the guide from this: https://wizardofodds.com/games/blackjack/strategy/calculator/

---
+++ AboutThisGame.java
@@ -0,0 +1,15 @@
+/*
+// File:             AboutThisGame.java
+// Created:          2018/04/04
+// Author:           danIv (Daniel Ivanovich)
+// Description:      Tell the user about the game.
+*/
+
+
+public class AboutThisGame {
+  public static void main (String[] args){
+    System.out.println("About Blackjack");
+    System.out.println("Authors: Armaan Vasowalla & Daniel Ivanovich");
+    System.out.println("Version: 1.0.0");
+  }
+}
