08570b10385196619cef9129418d8eca90cf6140
Daniel Ivanovich <Soccer.Ivanovich@gmail.com>
2018-04-05 08:55:09 -0400

    Created SignlePlayer

---
+++ SinglePlayer.java
@@ -0,0 +1,13 @@
+/*
+// File:             SinglePlayer.java
+// Created:          2018/04/05
+// Author:           danIv
+// Description:      The single player game.
+*/
+
+
+public class SinglePlayer {
+  public static void main (String[] args){
+  
+  }
+}
